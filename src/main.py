06398adf4328880df7ba75312bb3267d7476d21c
sakdag <serhat.akdag@outlook.com>
2021-06-28 18:40:29 +0300

    Graph creation using networkx implemented.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ import src.preprocessing.preprocessing as prep
 
 if __name__ == '__main__':
     dirname = os.path.dirname(__file__)
-
     kinship_data_file_name = os.path.join(dirname, conf.KINSHIP_GRAPH_FILE_PATH)
 
     kinship_graph = prep.generate_graph_from_file(kinship_data_file_name)
--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,6 +1,29 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
 def generate_graph_from_file(file_name: str):
     graph_file = open(file_name, 'r')
     lines = graph_file.readlines()
 
+    kinship_graph = nx.DiGraph()
+    already_added_nodes = set()
+
     for line in lines:
-        pass
+        if line == '\n':
+            continue
+        relationship = line[:line.index('(')]
+        split_by_comma = line.split(',')
+        first_node = split_by_comma[0][line.index('(') + 1:]
+        second_node = split_by_comma[1][:-2]
+
+        if first_node not in already_added_nodes:
+            kinship_graph.add_node(first_node)
+        if second_node not in already_added_nodes:
+            kinship_graph.add_node(second_node)
+        kinship_graph.add_edges_from([(first_node, second_node, {'label': relationship})])
+
+    # nx.draw(kinship_graph, with_labels=True)
+    # plt.show()
+
+    return kinship_graph
